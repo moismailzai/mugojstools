c2fc9d49bafdfd398392b8bdc8e041d4f5464a67
Mo Ismailzai <mo@mugo.ca>
2016-10-22 23:03:56 -0700

    added documentation.

--- classes/mugo_js_tools.php
+++ classes/mugo_js_tools.php
@@ -1,21 +1,54 @@
 <?php
+/**
+ * This extension provides JavaScript template operators for eZ Publish template files.
+ * See https://github.com/moismailzai/mugojstools
+ */
 class MugoJSTools
 {
+    /**
+     * The array of operators, used for registering operators.
+     * @var array
+     */
+    private $Operators;
+
+    /**
+     * mjs_console_log: This operator generates a JavaScript console.log() dump in the browser console. Supports pipes
+     *                  and can take any variable or literal input, including strings, objects, and ez nodes.
+     */
     function __construct()
     {
         $this->Operators = array( 'mjs_console_log' );
     }
 
+    /**
+     * Returns the template operators
+     */
     function operatorList()
     {
         return $this->Operators;
     }
 
+    /**
+     * Returns true to tell the template engine that the parameter list exists per operator type.
+     * @return boolean
+    */
     function namedParameterPerOperator()
     {
         return true;
     }
 
+    /**
+     * Returns an array of named parameters, this allows for easier retrieval of operator parameters. This also requires
+     * the function modify() has an extra parameter called $namedParameters. The position of each element (starts at 0)
+     * represents the position of the original sequenced parameters. The key of the element is used as parameter name,
+     * while the contents define the type and requirements.
+     *
+     * The keys of each element content is:
+     *      type:       defines the type of parameter allowed
+     *      required:   boolean which says if the parameter is required or not, if missing and required an error is
+     *                  displayed
+     *      default:    the default value if the parameter is missing
+    */
     function namedParameterList()
     {
         return array(
@@ -28,7 +61,21 @@ class MugoJSTools
         );
     }
 
-    function modify( $tpl, $operatorName, $operatorParameters, $rootNamespace,$currentNamespace, &$operatorValue, $namedParameters )
+    /**
+     * Modifies the input variable $operatorValue and sets the output result in the same variable.
+     *
+     * @param   object    $tpl	                The template object which called this class
+     * @param   string    $operatorName	        The name of this operator
+     * @param   array     $operatorParameters	The parameters for this operator
+     * @param   string    $rootNamespace	    The namespace which this operator works in
+     * @param   string    $currentNamespace	    The current namespace for functions, this is usually used in functions
+     *                                          for setting new variables
+     * @param   mixed     $operatorValue	    The input/output value
+     * @param   object    $namedParameters	    The parameters as named lookups, only required if namedParameterList()
+     *                                          is defined. The values of each parameter is also fetched for you
+     * @return  mixed     $operatorValue
+     */
+    function modify( $tpl, $operatorName, $operatorParameters, $rootNamespace, $currentNamespace, &$operatorValue, $namedParameters )
     {
         switch ($operatorName) {
             case "mjs_console_log":
@@ -42,6 +89,18 @@ class MugoJSTools
         }
     }
 
+    /**
+     * Returns a <script> tag which prints the contents of $logContent to the browser console using console.log().
+     *
+     * When $logContent is an EZ node, the following template is used:
+     *           {
+     *               objectID#: json_encode($logContent),
+     *                 nodeID#: ezjscAjaxContent::nodeEncode($logContent)
+     *           }
+     *
+     * @param mixed $logContent
+     * @return string $scriptTag
+     */
     function mjs_console_log( $logContent)
     {
         $jsonLogContent = json_encode($logContent, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK, 512);
@@ -76,5 +135,4 @@ EOS;
         }
         return $scriptTag;
     }
-    private $Operators;
 }
